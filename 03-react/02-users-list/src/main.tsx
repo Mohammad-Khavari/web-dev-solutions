613a6acd510b48030b5cb692adf75f2e4b9d02f4
Mohammad Khavari <61501015+Mohammad-Khavari@users.noreply.github.com>
2021-06-10 11:40:14 +0200

    Setup

---
+++ 03-react/02-users-list/src/main.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+const appDiv = document.getElementById("app");
+
+if (!(appDiv instanceof HTMLDivElement)) {
+	throw new Error("No div with id 'app' found");
+}
+
+
+const App = () => (
+	<div> </div>
+);
+
+
+ReactDOM.render(<App />, appDiv);
