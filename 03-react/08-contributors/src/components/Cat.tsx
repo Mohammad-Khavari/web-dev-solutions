663da9b5683b2652f56eab013ce335cbebfeafb6
Mohammad Khavari <61501015+Mohammad-Khavari@users.noreply.github.com>
2021-07-21 12:28:02 +0200

    Small exercise to create list of the gitHub users using gitHub API and show them on the page with specific infos

---
+++ 03-react/08-contributors/src/components/Cat.tsx
@@ -0,0 +1,6 @@
+  
+import * as React from "react";
+
+export default function Cat() {
+	return <img src="https://cataas.com/cat/gif" />;
+}
---
+++ 03-react/08-contributors/src/components/Home.tsx
@@ -0,0 +1,6 @@
+  
+import * as React from "react";
+
+export default function Home() {
+	return <p>Welcome to my awesome website!</p>;
+}
